7e1dc55b67ff18b1f11a92e2637329f92da2679f
The Doctor <learicist@gmail.com>
2018-06-18 15:42:03 -0400

    First git commit for project

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
 	$.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", 
 		function(json) {
-        	$("#quote").html(json.quoteText);
+         	$("#quote").html(json.quoteText);
 			$("#author").html(json.quoteAuthor);
-		});
+		}
+	);
+	
 	function colorChange() {
 		var colors = [
 			{
@@ -32,30 +34,42 @@ $(document).ready(function() {
 				body: "#18b8ad",
 			},
 		];
+		
 		var colorRandom = Math.floor(Math.random()*colors.length);
+		
 		$('.main').css('background-color', colors[colorRandom].main);
 		$('body').css('background-color', colors[colorRandom].body);
 		$('.textbox').css('color', colors[colorRandom].text);
 	}
+	
+	
 	$("#generate").on("click", function(json){
+		
 		$('body').fadeOut(1);
 		$('body').fadeIn(1100);
+		
 		$.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", 
 			function(json) {
-        		$("#quote").html(json.quoteText);
+         		$("#quote").html(json.quoteText);
 				$("#author").html(json.quoteAuthor);
 			});
+			
 		colorChange();
+		
 		});
+		
 	$("#tweet").on("click", function(){
 		var quote = $("#quote").text();
 		var author = $("#author").text();
 		var currentQuote = (quote + " -" + author);
+		
 		if (currentQuote.length > 140) {
+			
 			var arrQuote = quote.split("");
 			var tweetQuote = arrQuote.slice(0, 112).join("");
 			currentQuote = (tweetQuote + "... -" + author);
 		}
+		
 		window.open("https://twitter.com/intent/tweet?text=" + currentQuote);
 	});
 	
