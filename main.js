b3ce8d6cfaf22a88b93d1eafa8edb69cfa57fde2
The Doctor <learicist@gmail.com>
2022-06-25 01:53:15 -0400

    Attempt to update colors and behavior

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
-	$.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", 
+	$.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?",
 		function(json) {
          	$("#quote").html(json.quoteText);
 			$("#author").html(json.quoteAuthor);
 		}
 	);
-	
+
 	function colorChange() {
 		var colors = [
 			{
-				text: "#65abd1", 
-				main: "#d1a465", 
+				text: "#65abd1",
+				main: "#d1a465",
 				body: "#d16e65"
 			},
-			{
+			/*{
 				text: "#8730ba",
 				main: "#a8ba30",
 				body: "#ba7e30",
@@ -27,50 +27,54 @@ $(document).ready(function() {
 				text: "#23d63e",
 				main: "#d68323",
 				body: "#d623ca",
-			},
+			},*/
 			{
 				text: "#d2db4e",
 				main: "#db534e",
 				body: "#584edb",
 			},
 		];
-		
+
 		var colorRandom = Math.floor(Math.random()*colors.length);
-		
+
+		console.log(colorRandom);
+
 		$('.main').css('background-color', colors[colorRandom].main);
 		$('body').css('background-color', colors[colorRandom].body);
 		$('.textbox').css('color', colors[colorRandom].text);
+
 	}
-	
-	
-	$("#generate").on("click", function(json){
-		
+
+
+	$("#generate").on("click", function(json) {
+
 		$('body').fadeOut(1);
+		//set timeOut() before fadeIn or they'll happen together
 		$('body').fadeIn(1100);
-		
-		$.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", 
+
+		$.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?",
 			function(json) {
          		$("#quote").html(json.quoteText);
 				$("#author").html(json.quoteAuthor);
 			});
-			
+
 		colorChange();
-		
+
 		});
-		
+
 	$("#tweet").on("click", function(){
 		var quote = $("#quote").text();
 		var author = $("#author").text();
-		var currentQuote = (quote + " -" + author);
-		
+		var currentQuote = (quote + " - " + author);
+
 		if (currentQuote.length > 140) {
-			
+
 			var arrQuote = quote.split("");
 			var tweetQuote = arrQuote.slice(0, 112).join("");
-			currentQuote = (tweetQuote + "... -" + author);
+			currentQuote = (tweetQuote + "... - " + author);
 		}
-		
+
 		window.open("https://twitter.com/intent/tweet?text=" + currentQuote);
 	});
-	
+
 });
